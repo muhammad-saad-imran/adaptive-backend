94b25a998a03af3b600ef387a5321c9855bb42a7
Muhammad Saad Imran <muhammad.saad02@A006-00939.local>
2024-10-16 14:10:48 +0500

    chore: parse quote pipe changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { QuoteModule } from './insurance/quote/quote.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Quote } from 'src/insurance/quote/quote.entity';
 import { Insured } from 'src/insurance/insured/insured.entity';
-import { InsuredModule } from 'src/insurance/insured/insured.module';
 import { InsuranceModule } from './insurance/insurance.module';
 
 @Module({
@@ -23,7 +19,5 @@ import { InsuranceModule } from './insurance/insurance.module';
     }),
     ConfigModule.forRoot({ isGlobal: true }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
